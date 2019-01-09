f88624ed2d19110867e7e010f0c08009d351bc21
rskupnik <rskupnik@pgs-soft.com>
2019-01-09 10:35:18 +0100

    Add the onX methods to client mock

--- src/main/java/com/pgssoft/HttpClientMock.java
+++ src/main/java/com/pgssoft/HttpClientMock.java
@@ -5,6 +5,7 @@ import com.pgssoft.action.SetBodyStringAction;
 import com.pgssoft.action.SetStatusAction;
 import com.pgssoft.condition.BodyCondition;
 import com.pgssoft.rule.Rule;
+import com.pgssoft.rule.RuleBuilder;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLParameters;
@@ -29,10 +30,159 @@ import static org.hamcrest.Matchers.containsString;
 public final class HttpClientMock extends HttpClient {
 
     private final List<Rule> rules = new ArrayList<>();
+    private final List<RuleBuilder> rulesUnderConstruction = new ArrayList<>();
     private final String host;
 
     public HttpClientMock(String host) {
         this.host = host;
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP POST method.
+     *
+     * @return HttpClientMockBuilder which allows  to define new rule
+     */
+    public HttpClientMockBuilder onPost() {
+        return newRule("POST");
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP GET method.
+     *
+     * @return HttpClientMockBuilder which allows  to define new rule
+     */
+    public HttpClientMockBuilder onGet() {
+        return newRule("GET");
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP DELETE method.
+     *
+     * @return HttpClientMockBuilder which allows  to define new rule
+     */
+    public HttpClientMockBuilder onDelete() {
+        return newRule("DELETE");
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP HEAD method.
+     *
+     * @return HttpClientMockBuilder which allows  to define new rule
+     */
+    public HttpClientMockBuilder onHead() {
+        return newRule("HEAD");
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP OPTION method.
+     *
+     * @return HttpClientMockBuilder which allows  to define new rule
+     */
+    public HttpClientMockBuilder onOption() {
+        return newRule("OPTION");
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP PUT method.
+     *
+     * @return HttpClientMockBuilder which allows  to define new rule
+     */
+    public HttpClientMockBuilder onPut() {
+        return newRule("PUT");
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP PATCH method.
+     *
+     * @return HttpClientMockBuilder which allows  to define new rule
+     */
+    public HttpClientMockBuilder onPatch() {
+        return newRule("PATCH");
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP GET method and url. If provided url starts with "/" request url must be equal to concatenation of default
+     * host and url. Otherwise request url must equal to provided url. If provided url contains query parameters and/or reference they are parsed and added as a
+     * separate conditions. <p> For example:<br> <code> httpClientMock.onGet("http://localhost/login?user=Ben#edit"); </code> <br>is equal to<br> <code>
+     * httpClientMock.onGet("http://localhost/login").withParameter("user","Ben").withReference("edit); </code>
+     *
+     * @param url required url
+     * @return HttpClientMockBuilder which allows to define new rule
+     */
+    public HttpClientMockBuilder onGet(String url) {
+        return newRule("GET", url);
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP POST method and url. URL works the same way as in {@link #onGet(String) onGet}
+     *
+     * @param url required url
+     * @return HttpClientMockBuilder which allows to define new rule
+     */
+    public HttpClientMockBuilder onPost(String url) {
+        return newRule("POST", url);
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP PUT method and url. URL works the same way as in {@link #onGet(String) onGet}
+     *
+     * @param url required url
+     * @return HttpClientMockBuilder which allows to define new rule
+     */
+    public HttpClientMockBuilder onPut(String url) {
+        return newRule("PUT", url);
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP DELETE method and url. URL works the same way as in {@link #onGet(String) onGet}
+     *
+     * @param url required url
+     * @return HttpClientMockBuilder which allows to define new rule
+     */
+    public HttpClientMockBuilder onDelete(String url) {
+        return newRule("DELETE", url);
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP HEAD method and url. URL works the same way as in {@link #onGet(String) onGet}
+     *
+     * @param url required url
+     * @return HttpClientMockBuilder which allows to define new rule
+     */
+    public HttpClientMockBuilder onHead(String url) {
+        return newRule("HEAD", url);
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP OPTIONS method and url. URL works the same way as in {@link #onGet(String) onGet}
+     *
+     * @param url required url
+     * @return HttpClientMockBuilder which allows to define new rule
+     */
+    public HttpClientMockBuilder onOptions(String url) {
+        return newRule("OPTIONS", url);
+    }
+
+    /**
+     * Starts defining new rule which requires HTTP PATCH method and url. URL works the same way as in {@link #onGet(String) onGet}
+     *
+     * @param url required url
+     * @return HttpClientMockBuilder which allows to define new rule
+     */
+    public HttpClientMockBuilder onPatch(String url) {
+        return newRule("PATCH", url);
+    }
+
+    private HttpClientMockBuilder newRule(String method) {
+        RuleBuilder r = new RuleBuilder(method);
+        rulesUnderConstruction.add(r);
+        return new HttpClientMockBuilder(r);
+    }
+
+    private HttpClientMockBuilder newRule(String method, String url) {
+        RuleBuilder r = new RuleBuilder(method, host, url);
+        rulesUnderConstruction.add(r);
+        return new HttpClientMockBuilder(r);
     }
 
     @Override
